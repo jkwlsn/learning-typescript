f8669d41d6d26a874aa9f54dbbd8513294fb4658
Jake Wilson <hi@jkwlsn.dev>
2025-08-12 14:04:32 +0100

    Updates index.js

--- index.js
+++ index.js
@@ -1,16 +1,70 @@
-'use strict';
-var now = new Date();
-var UserAccount = /** @class */ (function () {
-    function UserAccount(user_id, email, password, timestamp) {
+"use strict";
+// Interfaces
+class UserAccount {
+    #user_id = 0;
+    #email = '';
+    #password = '';
+    #timestamp = '';
+    constructor(user_id, email, password, timestamp) {
         this.user_id = user_id;
         this.email = email;
         this.password = password;
         this.timestamp = timestamp.toISOString();
     }
-    return UserAccount;
-}());
-var InspectionRecord = /** @class */ (function () {
-    function InspectionRecord(inspection_id, timestamp, apiary_id, colony_id, queenright, queen_marked, queen_clipped, queen_cups, brood_frames, store_frames, room_frames, health, varroa, temper, feed, supers, weather, user_id) {
+    set user_id(user_id) {
+        this.#user_id = user_id;
+    }
+    set email(email) {
+        this.#email = email;
+    }
+    set password(password) {
+        this.#password = password;
+    }
+    set timestamp(timestamp) {
+        this.#timestamp = timestamp;
+    }
+    get user_id() {
+        return this.#user_id;
+    }
+    get email() {
+        return this.#email;
+    }
+    get password() {
+        return this.#password;
+    }
+    get timestamp() {
+        return this.#timestamp;
+    }
+    toObject(index) {
+        return {
+            index,
+            user_id: this.user_id,
+            email: this.email,
+            password: this.password,
+            timestamp: this.timestamp,
+        };
+    }
+}
+class InspectionRecord {
+    inspection_id;
+    timestamp;
+    apiary_id;
+    colony_id;
+    queenright;
+    queen_marked;
+    queen_clipped;
+    queen_cups;
+    brood_frames;
+    store_frames;
+    room_frames;
+    health;
+    varroa;
+    temper;
+    feed;
+    supers;
+    weather;
+    user_id;
+    constructor(inspection_id, timestamp, apiary_id, colony_id, queenright, queen_marked, queen_clipped, queen_cups, brood_frames, store_frames, room_frames, health, varroa, temper, feed, supers, weather, user_id) {
         this.inspection_id = inspection_id;
         this.timestamp = timestamp.toISOString();
         this.apiary_id = apiary_id;
@@ -30,95 +84,95 @@ var InspectionRecord = /** @class */ (function () {
         this.weather = weather;
         this.user_id = user_id;
     }
-    return InspectionRecord;
-}());
-var Users = /** @class */ (function () {
-    function Users() {
-        this.users = [];
-    }
-    Users.prototype.addUser = function (newUser) {
-        var user_id = Math.floor(Math.random() * 100);
-        var timestamp = now;
-        var user = new UserAccount(user_id, newUser.email, newUser.password, timestamp);
+}
+class Users {
+    users = [];
+    addUser(newUser) {
+        const user_id = Math.floor(Math.random() * 100);
+        const timestamp = new Date();
+        const user = new UserAccount(user_id, newUser.email, newUser.password, timestamp);
         this.users.push(user);
         return user;
-    };
-    return Users;
-}());
-var Records = /** @class */ (function () {
-    function Records() {
-        this.records = [];
     }
-    Records.prototype.addRecord = function (newInspection) {
-        var inspection_id = Math.floor(Math.random() * 100);
-        var inspection = new InspectionRecord(inspection_id, newInspection.timestamp, newInspection.apiary_id, newInspection.colony_id, newInspection.queenright, newInspection.queen_marked, newInspection.queen_clipped, newInspection.queen_cups, newInspection.brood_frames, newInspection.store_frames, newInspection.room_frames, newInspection.health, newInspection.varroa, newInspection.temper, newInspection.feed, newInspection.supers, newInspection.weather, newInspection.user_id);
+}
+class Records {
+    records = [];
+    addRecord(newInspection) {
+        const inspection_id = Math.floor(Math.random() * 100);
+        const inspection = new InspectionRecord(inspection_id, newInspection.timestamp, newInspection.apiary_id, newInspection.colony_id, newInspection.queenright, newInspection.queen_marked, newInspection.queen_clipped, newInspection.queen_cups, newInspection.brood_frames, newInspection.store_frames, newInspection.room_frames, newInspection.health, newInspection.varroa, newInspection.temper, newInspection.feed, newInspection.supers, newInspection.weather, newInspection.user_id);
         this.records.push(inspection);
         return inspection;
-    };
-    return Records;
-}());
-var userList = new Users();
-var recordsList = new Records();
-var user1 = userList.addUser({
-    email: 'jake@example.com',
-    password: 'testpassword',
+    }
+}
+const userList = new Users();
+const recordsList = new Records();
+// Form controller
+const formController = {
+    getUserData() {
+        const emailInput = document.getElementById('email_input');
+        if (!emailInput)
+            throw new Error('Email input not found');
+        const email = emailInput.value;
+        const passwordInput = document.getElementById('password_input');
+        if (!passwordInput)
+            throw new Error('password input not found');
+        const password = passwordInput.value;
+        return userList.addUser({ email: email, password: password });
+    },
+    delUserData() {
+        const indexInput = document.getElementById('index_input');
+        if (!indexInput)
+            throw new Error('Index input not found');
+        const index = parseInt(indexInput.value);
+        return userList.users.splice(index, 1);
+    },
+};
+// Controller
+function add() {
+    formController.getUserData();
+    updateView();
+}
+function del() {
+    formController.delUserData();
+    updateView();
+}
+function updateView() {
+    const userAddInput = document.getElementById('user_add_form');
+    if (userAddInput) {
+        userAddInput.reset();
+    }
+    const table = document.getElementById('view_table');
+    const headerRow = document.getElementById('header_row');
+    if (table && headerRow) {
+        table.innerHTML = '';
+        table.appendChild(headerRow);
+    }
+    userList.users.forEach((entry, index) => {
+        const row = document.createElement('tr');
+        const obj = entry.toObject(index);
+        row.innerHTML = `
+        <td>${obj.index}</td>
+        <td>${obj.email}</td>
+        <td>${obj.password}</td>
+        <td>${obj.timestamp}</td>
+      `;
+        table.appendChild(row);
+    });
+}
+// View
+document.addEventListener('DOMContentLoaded', function () {
+    const userAddForm = document.getElementById('user_add_form');
+    if (userAddForm) {
+        userAddForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            add();
+        });
+    }
+    const userDelForm = document.getElementById('user_del_form');
+    if (userDelForm) {
+        userDelForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            del();
+        });
+    }
 });
-var inspection1 = recordsList.addRecord({
-    timestamp: now,
-    apiary_id: 1,
-    colony_id: 1,
-    queenright: true,
-    queen_marked: 'yellow',
-    queen_clipped: true,
-    queen_cups: 2,
-    brood_frames: 5,
-    store_frames: 6,
-    room_frames: 1,
-    health: 'good',
-    varroa: 10,
-    temper: 5,
-    feed: 1,
-    supers: 3,
-    weather: 'fine',
-    user_id: user1.user_id,
-});
-var inspection2 = recordsList.addRecord({
-    timestamp: now,
-    apiary_id: 1,
-    colony_id: 2,
-    queenright: true,
-    queen_marked: 'blue',
-    queen_clipped: true,
-    queen_cups: 2,
-    brood_frames: 5,
-    store_frames: 6,
-    room_frames: 1,
-    health: 'good',
-    varroa: 10,
-    temper: 5,
-    feed: 1,
-    supers: 3,
-    weather: 'fine',
-    user_id: user1.user_id,
-});
-var inspection3 = recordsList.addRecord({
-    timestamp: now,
-    apiary_id: 2,
-    colony_id: 1,
-    queenright: true,
-    queen_marked: 'red',
-    queen_clipped: true,
-    queen_cups: 2,
-    brood_frames: 5,
-    store_frames: 6,
-    room_frames: 1,
-    health: 'good',
-    varroa: 10,
-    temper: 5,
-    feed: 1,
-    supers: 3,
-    weather: 'fine',
-    user_id: user1.user_id,
-});
-console.table(userList.users);
-console.table(recordsList.records);
