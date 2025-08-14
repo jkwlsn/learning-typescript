0e2faa5e3a6d0229c1263659f023b858b5821b28
Jake Wilson <hi@jkwlsn.dev>
2025-08-14 10:28:06 +0100

    Fixes Eslint config
    The eslint config was a mess that didn't really work properly. I spent
    some time fixing the config so everything plays nicely.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,29 @@
-import js from '@eslint/js';
-import globals from 'globals';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
-export default defineConfig([
+import html from '@html-eslint/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default tseslint.config(
+  { ignores: ['index.js'] },
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  // HTML config
   {
-    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: globals.node },
+    ...html.configs['flat/recommended'],
+    files: ['**/*.html'],
+    rules: {
+      ...html.configs['flat/recommended'].rules,
+      '@html-eslint/no-multiple-h1': 'off',
+      '@html-eslint/indent': 'off',
+      '@html-eslint/no-extra-spacing-attrs': [
+        'error',
+        { enforceBeforeSelfClose: true },
+      ],
+      '@html-eslint/require-closing-tags': ['error', { selfClosing: 'always' }],
+      '@html-eslint/attrs-newline': 'off',
+    },
   },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
-  tseslint.configs.recommended,
-]);
+  // ESLINT CONFIG PRETTIER
+  eslintConfigPrettier,
+);
